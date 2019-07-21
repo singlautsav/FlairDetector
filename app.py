b5d82abd031c43e562fcf6ebf2e9e31059f5948a
Utsav <usingla78@gmail.com>
2019-07-21 23:50:50 +0530

    mongo Added

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from redditFIles.reddit import getRed
 import pickle
-
+import pymongo
 # import pandas as pd
 import numpy as np
 
-from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-    name = "Utsav"
-    return render_template('index.html',name = name)
-
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/',methods = ['POST','GET'])
 def result():
-   if request.method == 'POST':
+    if request.method == 'POST':
         link = request.form['name']
-        print(link)
-        print(type(link))
         result = getRed(link)
         model = pickle.load(open("modelTCBU-c2.sav",'rb'))
         result = model.predict([result])[0]
-        print(result)
-        return result
+        return render_template('index.html',result=result)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
+    uri = 'mongodb://<dbuser>:<dbpassword>@ds253567.mlab.com:53567/heroku_kr16tlsv'
+    client = pymongo.MongoClient(uri)
+    db = client.get_default_database()
     app.run(debug = True)
